2874785ff172e9f27217d09cff004fad7a2cf0ab
daniielaCR <naniscr0416@gmail.com>
2023-08-16 15:48:57 -0500

    Commit

--- app/src/main/java/co/edu/sena/basicscodelab/MainActivity.kt
+++ app/src/main/java/co/edu/sena/basicscodelab/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.BackgroundServiceStartNotAllowedException
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -16,8 +17,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -43,6 +47,34 @@ class MainActivity : ComponentActivity() {
 }//MainActivity
 
 @Composable
+fun OnboardingScreen(modifier: Modifier = Modifier) {
+    // TODO: This state should be hoisted
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = { shouldShowOnboarding = false }
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen()
+    }
+}
+
+@Composable
 private fun Myapp(modifier: Modifier = Modifier, names: List<String> = listOf("World","Compose")){
     Column(modifier = modifier.padding(vertical = 4.dp)) {
         for (name in names){
@@ -61,10 +93,11 @@ private fun Myapp(modifier: Modifier = Modifier, names: List<String> = listOf("W
 fun MessageCard(name: String) {
 
     val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
 
     Surface(color = MaterialTheme.colorScheme.primary, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column (modifier = Modifier.weight(1f) /*modifier = Modifier.fillMaxWidth().padding()*/) {
+            Column (modifier = Modifier.weight(1f).padding(bottom = extraPadding) /*modifier = Modifier.fillMaxWidth().padding()*/) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }//Column
